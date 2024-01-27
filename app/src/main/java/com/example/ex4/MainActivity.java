bd9fdaa14a8e4e57f6ff749f9d178f937762876e
Shon Martinov <shon4010@gmail.com>
2024-01-27 22:47:55 +0200

    Finished

--- app/src/main/java/com/example/ex4/MainActivity.java
+++ app/src/main/java/com/example/ex4/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.ex4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
+    TextView tv;
+    int num = 0;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.button);
+        tv = (TextView) findViewById(R.id.textView);
+        btn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                num++;
+                if (num % 7 == 0 || num % 10 == 7) {
+                    tv.setText("Boom!");
+                } else {
+                    tv.setText(String.valueOf(num));
+                }
+            }
+        });
     }
 }
